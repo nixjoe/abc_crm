cd25f7c956af9caaec28c3a429f1eac87875a3fa
gengyongjiang <yongjiangbuaa@gmail.com>
2015-12-03 18:10:20 +0800

    组织分页视图及客户信息待加载jqyery

---
+++ admincp/control/tiger.php
@@ -0,0 +1,55 @@
+<?php
+!defined('APP_ROOT') && exit('Access Denied');
+class tigercontrol extends base {
+    public function __construct() {
+        parent::__construct();
+        $server_id = intval($_COOKIE['sid']);//指向tiger库
+		$this -> db = "`tiger`";
+    }
+
+    public function onuser() {
+        $sql = 'select * from user limit 200';
+        $res = load(MODEL_TIGER_USER) -> executeQuery($sql);
+        $GLOBALS['view_datas'] = array('res' => $res);
+    }
+    
+    public function onuser2() {        
+        //以下组织分页数据
+        $type = $this->input('type');
+		if($type == 'list') {
+			$page = $_GET['page'];
+			if($_GET['rows'] == '') {
+				$limit = 10;
+			} else {
+				$limit = $_GET['rows']; 
+			}
+			$sidx = $_GET['sidx']; // get index row - i.e. user click to sort 
+			$sord = $_GET['sord']; // get the direction 
+			if(!$sidx) $sidx =1; // connect to the database 
+
+			$count = count($transactions); 
+			if($count >0) {
+				$total_pages = ceil($count/$limit);
+			} else {
+				$total_pages = 0; 
+			} 
+			if ($page > $total_pages) $page=$total_pages; 
+			$start = $limit*$page - $limit; // do not put $limit*($page - 1)
+			if($start < 0) $start = 0;
+				$SQL = "SELECT * FROM user WHERE  ORDER BY $sidx $sord LIMIT $start , $limit"; 
+				$transactions = $this->load(MODEL_TIGER_USER)->executeQuery($SQL);
+			$response['page'] = $page; 
+			$response['total'] = $total_pages;
+			$response['records'] = $count; 
+			$i=0; 
+			foreach ($transactions as $transaction) {
+			 	$response['rows'][$i]['cell']=array($transaction['order_id'], $transaction['user_id'], $transaction['channel'], $transaction['channel_trans_id'],
+			 										$transaction['channel_user_id'], $transaction['credits'], $transaction['order_credits'],$transaction['money'], 
+			 										$transaction['currency'], $transaction['order_money'], $transaction['status'],
+			 										date('Y-m-d H:i:s', $transaction['timestamp']), date('Y-m-d H:i:s', $transaction['last_update']), time_to_day($transaction['timestamp'] - $useridvalid['createtime'])); 
+			 	$i++; 
+			}
+			return $response;
+    }
+}
+?>
--- tools/misc.php
+++ tools/misc.php
@@ -2,23 +2,32 @@
 //!defined('APP_ROOT') && exit('Access Denied');
 ini_set('memory_limit', '128M');
 ini_set('max_execution_time', 0);
-include "../config.inc.php";
+include '../config.inc.php';
 
-//xpub json
-$result=post_request('https://xpub.337.com/json.php',array('x2'=>123,'s'=>'gyjdxpubmm','c'=>'elex337_38170082','d'=>'234','force'=>'1'));
-echo $result;
+//组织字段及表头
+//客户信息
+$cols[]='user_affiliation';
+$cols[]='user_name';
+$cols[]='realname';
+$cols[]='user_classify';
+$cols[]='user_attribute';
+$cols[]='email';
+$cols[]='phone';
+$cols[]='register_time';
+$cols[]='mt4';
+$cols[]='mt4_real';
+$cols[]='pay_time';
+$cols[]='pay_out';
+$cols[]='status';
+$cols[]='url_from';
 
-//publish config
-//$appid='274';
-//publish_cfg($appid);
-//var_export(load(MODEL_FBDATA)->getFromCache(sprintf(CK_XPUB_APPCFG , $appid)));
+foreach ($cols as $col)
+{
+  echo "{name:'$col',index:'$col', width:50},\n";     
+}
+                            
+echo array_keys($newarr);                            
 
-//改配置
-//$img = 'xxx.png';
-//for($i=1;$i<=12;$i++){
-//	$update_sql="update tasks_def set img={$img} where taskid={$i};";
-//	echo $update_sql.'\n';
-//}
 
 
 ?>
